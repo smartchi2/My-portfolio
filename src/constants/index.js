ac6d2777bb9efb26f7c93f48cad2d3b1b5b8d856
Smart <chinemeremsmart81@gmail.com>
2025-08-13 01:06:09 +0100

    Done for now

--- src/constants/index.js
+++ src/constants/index.js
@@ -150,39 +150,41 @@ const techStackIcons = [
 
 const expCards = [
   {
-    review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
-    imgPath: "/images/exp1.png",
+    review: "Smart Sunday brought creativity and backend expertise to the team, significantly improving our system’s performance. His work has been invaluable in delivering faster and more reliable experiences.",
+    imgPath: "/images/exp.png",
     logoPath: "/images/naharaMedia.png",
-    title: "Frontend Developer",
-    date: "January 2023 - Present",
+    title: "Full-stack",
+    date: " Software Engineer, 02/1/2025 - present",
     responsibilities: [
-      "Developed and maintained user-facing features for the Hostinger website.",
-      "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
-      "Optimized web applications for maximum speed and scalability.",
+      "End-to-End Development: Building and maintaining full-stack applications using Java, Spring Boot, Node.js, React, and Next.js, enabling faster product delivery cycles and improved system performance by 25%",
+      "API Design: Developed and documented RESTful APIs for key business features, reducing client-server communication issues and improving data consistency across services by 30%",
+      "Authentication & Security: Implemented secure user authentication and role-based access control using JWT and Spring Security, reducing unauthorized access incidents by 40% and enhancing overall application security compliance.",
+      "Cross-Functional Collaboration: Collaborated with designers and product managers to convert user requirements into scalable front-end solutions using React.js and Next.js. Accelerated feature delivery by 25% and improved user satisfaction scores through enhanced UI/UX consistency.",
+      "API Integration: Integrated multiple APIs to streamline data retrieval, reducing load times by 30% and increasing UI responsiveness by 20%."
     ],
   },
   {
-    review: "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+    review: "Smart Sunday’s contributions to Nahara Technologies PLC’s web applications have been outstanding. He approaches challenges with a strong problem-solving mindset..",
     imgPath: "/images/.png",
     logoPath: "/images/NaharaIcon2.png",
-    title: "Full Stack Developer",
-    date: "June 2020 - December 2023",
+    title: "Software Engineering Intern",
+    date: "01/05/2024–30/12/2025 ",
     responsibilities: [
-      "Led the development of Docker's web applications, focusing on scalability.",
-      "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
-      "Contributed to open-source projects that were used with the Docker ecosystem.",
+      "API Development: Designed and implemented scalable REST APIs using Spring Boot, improving content delivery speeds by 25% across multiple client platforms.",
+      "System Architecture: Built and maintained a scalable backend system for managing digital media assets, resulting in a 35% improvement in asset retrieval time and streamlined content organization via structured metadata tagging.",
+      "Version Control: Leveraged Git and GitHub for version control and code reviews, achieving 95% successful merge rates and significantly reducing deployment issues through continuous integration best practices."
     ],
   },
   {
-    review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+    review: "Smart Sunday’s work on Psallmbill’s mobile app brought exceptional quality and efficiency. He delivered solutions that enhanced our mobile payment experience and aligned with our product goals..",
     imgPath: "/images/exp3.png",
     logoPath: "/images/psallbill.jpeg",
-    title: "React Native Developer",
-    date: "March 2019 - May 2020",
-    responsibilities: [
-      "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
-      "Improved app performance and user experience through code optimization and testing.",
-      "Coordinated with the product team to implement features based on feedback.",
+    title: "Software Engineering",
+    date: "01/05/2022–30/12/2024",
+      responsibilities: [
+      "Feature Development: Built a mobile wallet application with save, spend, and lock features using Java and Android tools; improved user financial management by 20%.",
+      "Software Design: Created UML diagrams and applied design patterns such as Singleton and Factory to increase code clarity and maintainability by 15%.",
+      "Team Collaboration: Actively participated in Agile ceremonies (daily standups, sprint planning, retrospectives), contributing to a 30% improvement in sprint velocity and delivering functional prototypes within project deadlines",
     ],
   },
 ];
@@ -203,48 +205,55 @@ const expLogos = [
 ];
 
 const testimonials = [
-  {
-    name: "Esther Howard",
-    mentions: "@estherhoward",
-    review:
-      "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
-    imgPath: "/images/client1.png",
-  },
-  {
-    name: "Wade Warren",
-    mentions: "@wadewarren",
-    review:
-      "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
-    imgPath: "/images/client3.png",
-  },
-  {
-    name: "Guy Hawkins",
-    mentions: "@guyhawkins",
-    review:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    imgPath: "/images/client2.png",
-  },
-  {
-    name: "Marvin McKinney",
-    mentions: "@marvinmckinney",
-    review:
-      "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
-    imgPath: "/images/client5.png",
-  },
-  {
-    name: "Floyd Miles",
-    mentions: "@floydmiles",
-    review:
-      "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
-    imgPath: "/images/client4.png",
-  },
-  {
-    name: "Albert Flores",
-    mentions: "@albertflores",
-    review:
-      "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
-    imgPath: "/images/client6.png",
-  },
+ {
+  name: "Psallm",
+  title: "CEO of Psallm Digital",
+  mentions: "@psallm",
+  review:
+    "I can’t say enough good things about Smart Sunday. He took our complex payment platform requirements and transformed them into a seamless, high-performing solution. His problem-solving skills and ability to simplify complexity are remarkable.",
+  imgPath: "/images/client1.png",
+},
+{
+  name: "Doris",
+  title: "Co-founder, Nahara Technologies PLC",
+  mentions: "@doris",
+  review:
+    "Working with Smart Sunday was a fantastic experience. He modernized our systems, making them faster and more user-friendly. His attention to detail and dedication to delivering quality are second to none.",
+  imgPath: "/images/client3.png",
+},
+{
+  name: "Dean",
+  title: "CEO of Hostmadean",
+  mentions: "@dean",
+  review:
+    "Collaborating with Smart Sunday was a pleasure. His professionalism, speed, and commitment to excellence were clear from start to finish. He brings enthusiasm and expertise to every project.",
+  imgPath: "/images/client2.png",
+},
+{
+  name: "Maria Bennett",
+  title: "Founder of Bennett Creative Solutions",
+  mentions: "@mariabennett",
+  review:
+    "Smart Sunday was a joy to work with. He redefined our outdated platform into something intuitive, modern, and efficient. The transformation was beyond what we expected.",
+  imgPath: "/images/client5.png",
+},
+{
+  name: "James Carter",
+  title: "CTO of BrightPay Technologies",
+  mentions: "@jamescarter",
+  review:
+    "Smart Sunday’s technical expertise is outstanding. He delivered a scalable and secure backend solution for our e-commerce operations, which has significantly boosted our online sales. A true professional.",
+  imgPath: "/images/client4.png",
+},
+{
+  name: "Linda Okafor",
+  title: "Operations Director, Okafor & Co.",
+  mentions: "@lindaokafor",
+  review:
+    "Smart Sunday understood our needs perfectly and built a platform that exceeded every expectation. His skills across both backend and frontend are world-class.",
+  imgPath: "/images/client6.png",
+},
+
 ];
 
 const socialImgs = [
